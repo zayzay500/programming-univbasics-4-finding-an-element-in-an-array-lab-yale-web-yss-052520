13ff1adc10db8fc566634335c6bc4091dd7abd38
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-20 00:30:40 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,9 @@
+require 'pry'
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |index|
+    if array[index] == value_to_find
+      return index
+    end
+  end
+  nil
 end
