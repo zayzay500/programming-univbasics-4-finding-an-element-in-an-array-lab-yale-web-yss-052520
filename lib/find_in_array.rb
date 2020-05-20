84975b727711834fa732839fa9a93a127d1b7021
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-20 00:27:18 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-    if array[index] == value_to_find do
+    if array[index] == value_to_find
       return index
+    else
+      return nil
     end
   end
 end
